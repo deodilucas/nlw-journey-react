3e4e4f60bd816c43bb40c3a76d6c6e0dd58001d5
Deodilucas Souza <deodilucas@gmail.com>
2024-07-14 22:19:57 -0300

    NLW Journey

---
+++ src/pages/trip-details/important-links.tsx
@@ -0,0 +1,30 @@
+import { Link2, Plus } from "lucide-react";
+import { Button } from "../../components/button";
+
+export function ImportantLinks() {
+    return (
+        <div className="space-y-6">
+            <h2 className="font-semibold text-xl">Links Importantes</h2>
+            <div className="space-y-5">
+                <div className="flex items-center justify-between gap-4">
+                    <div className="space-y-1.5">
+                        <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
+                        <a className="block text-xs font-medium text-zinc-400 truncate hover:text-zinc-200">https://www.airbnb.com.br/rooms/104700011777777777799213000213</a>
+                    </div>
+                    <Link2 className="text-zinc-400 size-5 shrink-0" />
+                </div>
+                <div className="flex items-center justify-between gap-4">
+                    <div className="space-y-1.5">
+                        <span className="block font-medium text-zinc-100">Reserva do AirBnB</span>
+                        <a className="block text-xs font-medium text-zinc-400 truncate hover:text-zinc-200">https://www.airbnb.com.br/rooms/104700011777777777799213000213</a>
+                    </div>
+                    <Link2 className="text-zinc-400 size-5 shrink-0" />
+                </div>
+            </div>
+            <Button size="full" variant="secondary">
+                <Plus className="size-5" />
+                Cadastrar novo link
+            </Button>
+        </div>
+    )
+}
